b522529e0cbfb86c04785ed3e606360b28798eed
quocanh <quocanh1897@gmail.com>
2018-04-15 11:57:20 +0700

    frontend + backend init

---
+++ public/sources/site/js/cufon-replace.js
@@ -0,0 +1,3 @@
+Cufon.replace('#header .row-1 .fleft', { fontFamily: 'Gill Sans', color: '-linear-gradient(#bfbfbf, #919191)' });
+Cufon.replace('#header .row-1 .fleft span', { fontFamily: 'Gill Sans', color: '-linear-gradient(#d72a18, #b60803)' });
+Cufon.replace('#header .row-2 ul li, h2, h3, h4', { fontFamily: 'Gill Sans', hover:true });
---
+++ resources/views/master.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Home - Home Page | Cinema - Free Website Template from Templates.com</title>
+<base href="{{asset('')}}">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Place your description here" />
+<meta name="keywords" content="put, your, keyword, here" />
+<meta name="author" content="Templates.com - website templates provider" />
+<link href="sources/site/style.css" rel="stylesheet" type="text/css" />
+<script src="sources/site/js/jquery-1.4.2.min.js" type="text/javascript"></script>
+<script src="sources/site/js/cufon-yui.js" type="text/javascript"></script>
+<script src="sources/site/js/cufon-replace.js" type="text/javascript"></script>
+<script src="sources/site/js/Gill_Sans_400.font.js" type="text/javascript"></script>
+<script src="sources/site/js/script.js" type="text/javascript"></script>
+<!--[if lt IE 7]>
+	<script type="text/javascript" src="js/ie_png.js"></script>
+	<script type="text/javascript">
+		 ie_png.fix('.png, .link1 span, .link1');
+	</script>
+	<link href="ie6.css" rel="stylesheet" type="text/css" />
+<![endif]-->
+</head>
+<body id="page1">
+<div class="tail-top">
+	<div class="tail-bottom">
+		<div id="main">
+<!-- HEADER -->
+			<div id="header">
+				<div class="row-1">
+					<div class="fleft"><a href="index.html">Cinema <span>World</span></a></div>
+					<ul>
+						<li><a href="sources/site/index.html"><img src="sources/site/images/icon1-act.gif" alt="" /></a></li>
+						<li><a href="sources/site/contact-us.html"><img src="sources/site/images/icon2.gif" alt="" /></a></li>
+						<li><a href="sources/site/sitemap.html"><img src="sources/site/images/icon3.gif" alt="" /></a></li>
+					</ul>
+				</div>
+				<div class="row-2">
+					<ul>
+						<li><a href="sources/site/index.html" class="active">Home</a></li>
+						<li><a href="sources/site/about-us.html">About</a></li>
+						<li><a href="sources/site/articles.html">Articles</a></li>
+						<li><a href="sources/site/contact-us.html">Contacts</a></li>
+						<li class="last"><a href="sources/site/sitemap.html">Sitemap</a></li>
+					</ul>
+				</div>
+			</div>
+<!-- CONTENT -->
+			<div id="content">
+				<div id="slogan">
+					<div class="image png"></div>
+					<div class="inside">
+						<h2>We are breaking<span>All Limitations</span></h2>
+						<p>Lorem ipsum dolor consectetur adipisicing elit, sed do eiusmod tempor incididunt labore etolore magna aliqua enim minim veniam quis nostrud exercitation ullamco laboris.</p>
+						<div class="wrapper"><a href="#" class="link1"><span><span>Learn More</span></span></a></div>
+					</div>
+				</div>
+				<div class="box">
+					<div class="border-right">
+						<div class="border-left">
+							<div class="inner">
+								<h3>Welcome to <b>Cinema</b> <span>World</span></h3>
+								<p>Cinema World Site is a free web template created by TemplateMonster.com team. This website template is optimized for 1024X768 screen resolution. It is also XHTML &amp; CSS valid.</p>
+								<div class="img-box1"><img src="sources/site/images/1page-img1.jpg" alt="" />This website template can be delivered in two packages - with PSD source files included and without them. If you need PSD source files, please go to the template download page at TemplateMonster to leave the e-mail address that you want the template ZIP package to be delivered to.</div>
+								<p>This website template has several pages: <a href="sources/site/index.html">Home</a>, <a href="about-us.html">About us</a>, <a href="articles.html">Articles</a> (with Article page), <a href="contact-us.html">Contact us</a> (note that contact us form – doesn’t work), <a href="sitemap.html">Site Map</a>.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="content">
+					<h3>Fresh <span>Movies</span></h3>
+					<ul class="movies">
+						<li>
+							<h4>Toy Story 3</h4><img src="sources/site/images/1page-img2.jpg" alt="" />
+							<p>Check out Disney-Pixar's official Toy Story site, watch the <span>Toy Story 3</span> trailer, and meet new characters. Remember, no toy gets left behind!</p>
+							<div class="wrapper"><a href="#" class="link2"><span><span>Read More</span></span></a></div>
+						</li>
+						<li>
+							<h4>Prince of Percia: Sands of Time</h4><img src="sources/site/images/1page-img3.jpg" alt="" />
+							<p>A young fugitive prince and princess must stop a villain who unknowingly threatens to destroy the world with a special dagger.</p>
+							<div class="wrapper"><a href="#" class="link2"><span><span>Read More</span></span></a></div>
+						</li>
+						<li class="last">
+							<h4>The Twilight Saga: Eclipse</h4><img src="sources/site/images/1page-img4.jpg" alt="" />
+							<p>As a string of mysterious killings grips Bella is forced to choose between her love for vampire Edward and her friendship with werewolf Jacob.</p>
+							<div class="wrapper"><a href="#" class="link2"><span><span>Read More</span></span></a></div>
+						</li>
+						<li class="clear">&nbsp;</li>
+					</ul>
+				</div>
+			</div>
+<!-- FOOTER -->
+			<div id="footer">
+				<div class="left">
+					<div class="right">
+						<div class="inside">Copyright - Type in your name here<br />
+							Website template designed by <a class="new_window" href="http://www.templatemonster.com/" target="_blank" rel="nofollow">www.templatemonster.com</a><br />
+							3D Models provided by <a class="new_window" href="http://www.templates.com/product/3d-models/" target="_blank" rel="nofollow">www.templates.com</a> 
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
+</html>
