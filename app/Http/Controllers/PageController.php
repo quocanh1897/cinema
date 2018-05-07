b8b6d9336d5136e1bcde75a1d0a019aed5dd89bc
Quoc Anh <quocanh1897@gmail.com>
2018-05-08 00:38:58 +0700

    pagecontroller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function getIndex()
+    public function getIndex()//lay trang chu
     {
         return view('master');
     }
