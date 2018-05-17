9555c0659eeba56ca89beed3f6081ec6709c9bc0
thanhduy <ntduy.cs@gmail.com>
2018-05-18 01:20:09 +0700

    edit hien thi phim dang/sap chieu o trang chu de phu hop khi co nhieu phim

--- resources/views/page/trangchu.blade.php
+++ resources/views/page/trangchu.blade.php
@@ -243,18 +243,17 @@
           <a id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false"
             class="nav-link ">PHIM SẮP CHIẾU</a>
         </li>
-
       </ul>
+
       <div id="pills-tabContent" class="tab-content"  >
         <div id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" class="tab-pane fade show active">
-
+         
           <div class="row portfolio text-center" >
             @foreach($new_phim as $new)
-
-            <div class="col-md-6">
+            <div class="col-md-auto item">
               <div class="box-image">
-                <div class="image">
-                  <img src="{{$new->hinhanh}} " alt="" class="img-fluid" height="100" width="300">
+                <div class="image-duy">
+                  <img src="{{$new->hinhanh}} " alt="" class="img-fluid" height="50" width="250">
                   <div class="overlay d-flex align-items-center justify-content-center">
                     <div class="content">
                       <div class="name">
@@ -278,14 +277,15 @@
             @endforeach
           </div>
         </div>
+
         <div id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" class="tab-pane fade">
           <div class="row portfolio text-center">
             @foreach($pre_phim as $pr)
 
-            <div class="col-md-6">
+            <div class="col-md-auto item">
               <div class="box-image">
                 <div class="image">
-                  <img src="{{$pr->hinhanh}} " alt="" class="img-fluid" height="100" width="300">
+                  <img src="{{$pr->hinhanh}} " alt="" class="img-fluid" height="50" width="250">
                   <div class="overlay d-flex align-items-center justify-content-center">
                     <div class="content">
                       <div class="name">
