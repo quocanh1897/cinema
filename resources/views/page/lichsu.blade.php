094a62c0a58d0be29380cbe82737047fa72d5912
thanhduy <ntduy.cs@gmail.com>
2018-05-19 02:14:54 +0700

    Chinh sua giao dien lich su giao dich

--- resources/views/page/lichsu.blade.php
+++ resources/views/page/lichsu.blade.php
@@ -5,7 +5,19 @@
             <div class="col-md-7">
               <h1 class="h2">Giao dịch của tôi</h1>
             </div>
-             
+            <div class="col-md-5">
+              <ul class="breadcrumb d-flex justify-content-end">
+                <li class="breadcrumb-item">
+                  <a href="index.html">Trang chủ</a>
+                </li>
+                <li class="breadcrumb-item active">
+                  <a href="profile">Tài khoản</a>
+                </li>
+                <li class="breadcrumb-item">
+                  Lịch sử giao dịch
+                </li>
+              </ul>
+            </div> 
           </div>
         </div>
       </div>
@@ -13,53 +25,64 @@
         <div class="container">
           <div class="row bar mb-0">
             <div id="customer-orders" class="col-md-9">
-              <p class="text-muted lead">If you have any questions, please feel free to <a href="contact.html">contact us</a>, our customer service center is working for you 24/7.</p>
+              <p class="text-muted lead">Bạn đã thực hiện 5 giao dịch.</p>
               <div class="box mt-0 mb-lg-0">
                 <div class="table-responsive">
                   <table class="table table-hover">
                     <thead>
                       <tr>
-                        <th>Order</th>
-                        <th>Date</th>
-                        <th>Total</th>
-                        <th>Status</th>
-                        <th>Action</th>
+                        <th>Mã giao dịch</th> <!-- cột mahoadon trong database -->
+                        <th>Ngày giao dịch</th>
+                        <th>Vé</th>
+                        <th>Dịch vụ</th>                        
+                        <th>Tổng tiền</th>
+                        <th>Chi tiết</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
                         <th># 1735</th>
                         <td>22/06/2013</td>
-                        <td>$ 150.00</td>
-                        <td><span class="badge badge-info">Being prepared</span></td>
+                        <td>2</td>
+                        <!-- <td><span class="badge badge-info">Being prepared</span></td> -->
+                        <td>1</td>
+                        <td>170,000</td>
                         <td><a href="{{route('chi-tiet-lich-su')}}" class="btn btn-template-outlined btn-sm">View</a></td>
                       </tr>
                       <tr>
                         <th># 1735</th>
                         <td>22/06/2013</td>
-                        <td>$ 150.00</td>
-                        <td><span class="badge badge-info">Being prepared</span></td>
+                        <td>1</td>
+                        <!-- <td><span class="badge badge-info">Being prepared</span></td> -->
+                        <td>1</td>
+                        <td>120,000</td>
                         <td><a href="{{route('chi-tiet-lich-su')}}" class="btn btn-template-outlined btn-sm">View</a></td>
                       </tr>
                       <tr>
                         <th># 1735</th>
                         <td>22/06/2013</td>
-                        <td>$ 150.00</td>
-                        <td><span class="badge badge-success">Received</span></td>
+                        <td>3</td>
+                        <td>2</td>
+                        <!-- <td><span class="badge badge-success">Received</span></td> -->
+                        <td>250,000</td>
                         <td><a href="{{route('chi-tiet-lich-su')}}" class="btn btn-template-outlined btn-sm">View</a></td>
                       </tr>
                       <tr>
                         <th># 1735</th>
                         <td>22/06/2013</td>
-                        <td>$ 150.00</td>
-                        <td><span class="badge badge-danger">Cancelled</span></td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>120,000</td>
+                        <!-- <td><span class="badge badge-danger">Cancelled</span></td> -->
                         <td><a href="{{route('chi-tiet-lich-su')}}" class="btn btn-template-outlined btn-sm">View</a></td>
                       </tr>
                       <tr>
                         <th># 1735</th>
                         <td>22/06/2013</td>
-                        <td>$ 150.00</td>
-                        <td><span class="badge badge-warning">On hold</span></td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>120,000</td>
+                        <!-- <td><span class="badge badge-warning">On hold</span></td> -->
                         <td><a href="{{route('chi-tiet-lich-su')}}" class="btn btn-template-outlined btn-sm">View</a></td>
                       </tr>
                     </tbody>
@@ -76,16 +99,16 @@
                     <div class="panel-body">
                         <ul class="nav nav-pills flex-column text-sm">
                             <li class="nav-item">
-                                <a  class="nav-link active">
+                                <a href="{{route('profile')}}" class="nav-link">
+                                    <i class="fa fa-user"></i> Thông tin cá nhân</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active">
                                     <i class="fa fa-list"></i> Lịch sử giao dịch</a>
                             </li>
                             <li class="nav-item">
-                                <a href="{{route('phim-da-xem')}}" class="nav-link">
+                                <a  href="{{route('phim-da-xem')}}" class="nav-link ">
                                     <i class="fa fa-heart"></i> Phim đã xem</a>
-                            </li>
-                            <li class="nav-item">
-                                <a  href="{{route('profile')}}" class="nav-link ">
-                                    <i class="fa fa-user"></i> Cài đặt</a>
                             </li>
 
                         </ul>
