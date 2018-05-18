bc7abbfe13bb4f661f6e368b48662b14c601081f
thanhduy <ntduy.cs@gmail.com>
2018-05-18 14:58:42 +0700

    edit giao dien chi tiet phim

--- resources/views/page/chitiet.blade.php
+++ resources/views/page/chitiet.blade.php
@@ -6,7 +6,17 @@
       <div class="col-md-7">
         <h1 class="h2">{{$phim->first()->tenphim}} </h1>
       </div>
-
+      <div class="col-md-5">
+        <ul class="breadcrumb d-flex justify-content-end">
+          <li class="breadcrumb-item">
+            <a href="index.html">Trang chủ</a>
+          </li>
+          <li class="breadcrumb-item">
+            <a href="phim-dang-chieu">Phim</a>
+          </li>
+          <li class="breadcrumb-item active">{{$phim->first()->tenphim}}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </div>
@@ -15,114 +25,92 @@
     <div class="row bar">
       <!-- LEFT COLUMN _________________________________________________________-->
       <div class="col-lg-18">
+        
+          <div id="productMain" class="row">
+            <!-- Poster phim -->
+            <div class="col-sm-4">
+              <div data-slider-id="1" class="owl-carousel shop-detail-carousel">
+                <div> <img src="{{$phim->first()->hinhanh}} " alt="" class="img-fluid"></div>
+              </div>
+            </div>
+            <!-- Chi tiet phim -->
+            <div class="col-sm-duy">
+              <div id="details" class="box mb-4 mt-4">
+                <style type="text/css">
+                th{
+                  padding: 10px;
+                  font-weight: normal;
+                  font-size: 16px;
+                }
+                th#thduy{
+                  color: gray ;
+                  font-weight: bold;
+                }
+              </style>
+              <table style="padding: 15px">
+                <tr>
+                  <th id="thduy">Đạo diễn</th>
+                  <th>{{$phim->first()->daodien}}</th>
+                </tr>
+                <tr>
+                  <th id="thduy">Thể loại</th>
+                  <th>
+                    @foreach($theloai as $th)
+                    {{$th->theloai}},
+                    @endforeach
+                  </th>                
+                </tr>
+                <tr>
+                  <th id="thduy">Diễn viên</th>
+                  <th>
+                    @foreach($dienvien as $dv)
+                    {{$dv->dienvien}},
+                    @endforeach
+                  </th>
+                </tr>
+                <tr>
+                  <th id="thduy">Khởi chiếu</th>
+                  <th>{{$phim->first()->batdau}}</th>
+                </tr>
+                <tr>
+                  <th id="thduy">Thời lượng</th>
+                  <th>{{$phim->first()->thoiluong}}</th>
+                </tr>
+                <tr>
+                  <th id="thduy">Đối tượng</th>
+                  <th>{{$phim->first()->doituong}}</th>
+                </tr>
+              </table>
+              <p>
+                <a href="{{route('chon-phim',$phim->first()->maphim)}}" class="btn btn-template-main">Đặt vé ngay</a>
+                <a href="{{$phim->first()->trailer}}" class="btn btn-template-main">Trailer</a>
+              </p>
+            </div>       
+          </div>
+          <!-- End chi tiet phim -->
+
+          <div class="col-md-duy">
+            <!-- PAGES MENU -->
+            <div class="panel panel-default sidebar-menu">
+              <div class="panel-heading">
+                <h3 class="h4 panel-title">Khuyến mãi</h3>
+              </div>            
+            </div>
+
+            <div class="banner">
+              <a href="index" class="text-center"><img src="sources/img/template-easy-code.png" alt="" class="img-fluid">Khuyến mãi 1</a>
+            </div>
+
+            
+
+          </div>
+
+        </div>
         <p class="lead">
           <b>Nội dung phim: </b>{{$phim->first()->mota}} </p>
-        <div id="productMain" class="row">
-          <div class="col-sm-4">
-            <div>
-              <br><br>
-              <img src="{{$phim->first()->hinhanh}} " alt="" class="img-fluid">
-            </div>
-            <p class="text-center">
-              <a href="{{route('chon-phim',$phim->first()->maphim)}}" class="btn btn-template-main">Đặt vé ngay</a>
-            </p>
-          </div>
-          <div class="col-sm-8">
-            <div id="details" class="box mb-4 mt-4">
-              <h4>Đạo diễn</h4>
-              <p style="padding-left: 1.8em">{{$phim->first()->daodien}}</p>
-              <h4>Diễn viên</h4>
-              <p style="padding-left: 1.8em"> 
-                @foreach($dienvien as $dv)
-                  {{$dv->dienvien}},
-
-                @endforeach
-              
-              </p>
-              <h4>Thể loại</h4>
-              <p style="padding-left: 1.8em">
-              @foreach($theloai as $th)
-                  {{$th->theloai}},
-
-                @endforeach
-              
-              </p>
-              <h4>Ngày khởi chiếu</h4>
-              <p style="padding-left: 1.8em">{{$phim->first()->batdau}}</p>
-              <h4>Thời lượng</h4>
-              <p style="padding-left: 1.8em">{{$phim->first()->thoiluong}}</p>
-              <h4>Đối tượng</h4>
-              <p style="padding-left: 1.8em">{{$phim->first()->doituong}}</p>
-            </div>
-
-          </div>
-        </div>
-<!--
-        <div id="product-social" class="box social text-center mb-5 mt-5">
-          <h4 class="heading-light">Chia sẻ với bạn bè</h4>
-          <ul class="social list-inline">
-            <li class="list-inline-item">
-              <a href="#" data-animate-hover="pulse" class="external facebook">
-                <i class="fa fa-facebook"></i>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#" data-animate-hover="pulse" class="external gplus">
-                <i class="fa fa-google-plus"></i>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#" data-animate-hover="pulse" class="external twitter">
-                <i class="fa fa-twitter"></i>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#" data-animate-hover="pulse" class="email">
-                <i class="fa fa-envelope"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-2 col-md-6">
-            <div class="box text-uppercase mt-0 mb-small">
-              <h3>Có thể bạn quan tâm</h3>
-            </div>
-          </div>
-          <div class="col-lg-2 col-md-6">
-            <div class="product">
-              <div class="image">
-                <a href="#">
-                  <img src="sources/img/product2.jpg" alt="" class="img-fluid image1">
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-2 col-md-6">
-            <div class="product">
-              <div class="image">
-                <a href="#">
-                  <img src="sources/img/product3.jpg" alt="" class="img-fluid image1">
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-2 col-md-6">
-            <div class="product">
-              <div class="image">
-                <a href="#">
-                  <img src="sources/img/product1.jpg" alt="" class="img-fluid image1">
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
--->
     </div>
   </div>
-</div>
 </div>
 
 @endsection
