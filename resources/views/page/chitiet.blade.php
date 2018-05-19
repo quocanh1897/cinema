032847dfdea05f700d7f53f9777dc744b737f81f
Quoc Anh <quocanh1897@gmail.com>
2018-05-19 23:14:40 +0700

    fix loi o index

--- resources/views/page/chitiet.blade.php
+++ resources/views/page/chitiet.blade.php
@@ -6,17 +6,7 @@
       <div class="col-md-7">
         <h1 class="h2">{{$phim->first()->tenphim}} </h1>
       </div>
-      <div class="col-md-5">
-        <ul class="breadcrumb d-flex justify-content-end">
-          <li class="breadcrumb-item">
-            <a href="index.html">Trang chủ</a>
-          </li>
-          <li class="breadcrumb-item">
-            <a href="phim-dang-chieu">Phim</a>
-          </li>
-          <li class="breadcrumb-item active">{{$phim->first()->tenphim}}</li>
-        </ul>
-      </div>
+      
     </div>
   </div>
 </div>
@@ -26,26 +16,28 @@
       <!-- LEFT COLUMN _________________________________________________________-->
       <div class="col-lg-18">
 
-        <p class="lead">
-          <b>Nội dung phim: </b>{{$phim->first()->mota}} </p>
         <div id="productMain" class="row">
-          <div class="col-sm-2">
-            <div>
-              <br><br>
-              <img src="{{$phim->first()->hinhanh}} " alt="" class="img-fluid">
-
+          <!-- Poster phim -->
+          <div class="col-sm-4">
+            <div data-slider-id="1" class="owl-carousel shop-detail-carousel">
+              <div>
+                <img src="{{$phim->first()->hinhanh}} " alt="" class="img-fluid">
+              </div>
             </div>
-            <!-- Chi tiet phim -->
-            <div class="col-sm-duy">
-              <div id="details" class="box mb-4 mt-4">
-                <style type="text/css">
-                th{
+
+          </div>
+          <!-- Chi tiet phim -->
+          <div class="col-sm-duy">
+            <div id="details" class="box mb-4 mt-4">
+              <style type="text/css">
+                th {
                   padding: 10px;
                   font-weight: normal;
                   font-size: 16px;
                 }
-                th#thduy{
-                  color: gray ;
+
+                th#thduy {
+                  color: gray;
                   font-weight: bold;
                 }
               </style>
@@ -57,17 +49,13 @@
                 <tr>
                   <th id="thduy">Thể loại</th>
                   <th>
-                    @foreach($theloai as $th)
-                    {{$th->theloai}},
-                    @endforeach
-                  </th>                
+                    @foreach($theloai as $th) {{$th->theloai}}, @endforeach
+                  </th>
                 </tr>
                 <tr>
                   <th id="thduy">Diễn viên</th>
                   <th>
-                    @foreach($dienvien as $dv)
-                    {{$dv->dienvien}},
-                    @endforeach
+                    @foreach($dienvien as $dv) {{$dv->dienvien}}, @endforeach
                   </th>
                 </tr>
                 <tr>
@@ -84,10 +72,13 @@
                 </tr>
               </table>
               <p>
-                <a href="{{route('chon-phim',$phim->first()->maphim)}}" class="btn btn-template-main">Đặt vé ngay</a>
+                <form action="{{route('chon-phim')}}" method="post">
+                  <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                  <button name="idphim" type="submit" value="{{$phim->first()->maphim}}" class="btn btn-template-main">Đặt vé ngay</button>
+                </form>
                 <a href="{{$phim->first()->trailer}}" class="btn btn-template-main">Trailer</a>
               </p>
-            </div>       
+            </div>
           </div>
           <!-- End chi tiet phim -->
 
@@ -96,14 +87,15 @@
             <div class="panel panel-default sidebar-menu">
               <div class="panel-heading">
                 <h3 class="h4 panel-title">Khuyến mãi</h3>
-              </div>            
+              </div>
             </div>
 
             <div class="banner">
-              <a href="index" class="text-center"><img src="sources/img/template-easy-code.png" alt="" class="img-fluid">Khuyến mãi 1</a>
+              <a href="index" class="text-center">
+                <img src="sources/img/template-easy-code.png" alt="" class="img-fluid">Khuyến mãi 1</a>
             </div>
 
-            
+
 
           </div>
 
